870177889069d27971251ecadc47e1b0c615fd3a
tanvir_bespoke <tanvir.bespoke@gmail.com>
2022-05-01 09:06:50 +0600

    added

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,12 @@
+namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name {get;set;}
+        public double Price {get;set;}
+        public bool IsActive {get;set;}
+
+        public IEnumerable<Demo_ProductProp> ProductProperties {get;set;}
+    }
+}
